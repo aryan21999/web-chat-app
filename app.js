3af0bfeef70cb4e82a500e7c75f05256997c0d4f
aryan21999 <itsaryangupta1999@gmail.com>
2021-09-24 15:37:16 +0530

    view-engine setup

--- app.js
+++ app.js
@@ -5,6 +5,34 @@ const db = require('./db/mongoose')
 const app = express();
 const port = process.env.port || 3000
 
+app.use(express.static(__dirname + '././public'));
+const publicDirectoryPath = path.join(__dirname, '/views/')
+
+app.set("view engine", "ejs");
+
+app.get('/', function (req, res, next) {
+  res.render(`${publicDirectoryPath}signin`)
+})
+
+
+app.get('/signup', function (req, res, next) {
+  res.render(`${publicDirectoryPath}signup`)
+})
+
+app.get('/forget', function (req, res, next) {
+    res.render(`${publicDirectoryPath}forget`)
+  })
+
+app.get('/addContact', function (req, res, next) {
+    res.render(`${publicDirectoryPath}addContact`)
+  })
+
+app.use(express.static(publicDirectoryPath))
+
+
+app.use(express.json())
+app.use(express.static("public"));
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
