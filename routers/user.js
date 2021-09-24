4866925b284f0bdfaac83716c418e12953895de6
aryan21999 <itsaryangupta1999@gmail.com>
2021-09-24 15:06:20 +0530

    update user js

--- routers/user.js
+++ routers/user.js
@@ -30,11 +30,42 @@ router.get('/reg/me', async (req, res) => {
     res.send(req.user)
 })
 
+router.patch('/reg/me', async (req, res) => {
+    const update = Object.keys(req.body)
+    const allowedUpdates = ['name', 'password']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+        if(!isValidOperation) {
+            return res.status(400).send({error})
+        }
+        try {
+            updates.forEach((update) => req.user[update] = req.body[update])
+            await req.user.save()
+            res.send(req.user)
+            console.log(user)
+        }
+        catch (e){
+            console.log(e)
+            res.status(400).send(e)
+        }
+})
+
+
 router.post('/reg/forget', async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['password']
         try {
             const user = User.findOneAndUpdate({email: req.body.email}, {password: req.body.password}, {new: true})
+            .then((newUser) => {
+                console.log(newUser)
+                res.status(200).send(newUser)
+            })
+            .catch((error) => {
+                return res.status(400).send("User With Given Email Doesn't Exist!" + error)
+            })
         }
-})
+        catch (error) {
+            console.log(error)
+            res.status(500).send(error)
+        }
+    })
 
