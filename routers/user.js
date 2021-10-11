147be5b8099400707ba69387b2e162d834e9a917
aryan21999 <itsaryangupta1999@gmail.com>
2021-10-11 12:45:12 +0530

    User Route

--- routers/user.js
+++ routers/user.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 
 const router = new express.Router()
 
+
+// SignUp
 router.post('/reg', async (req, res) => {
     const user = new User(req.body)
         try {
@@ -15,10 +18,12 @@ router.post('/reg', async (req, res) => {
         }
 })
 
+// LogIn
 router.post('/reg/login', async (req, res) => {
     try {
-        const user = await user.findOne({email: req.body.email, password: req.body.password})
-        res.send({ user })
+        const user = await User.findOne({ email: req.body.email, password: req.body.password })
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
     }
     catch (e) {
         console.log(e)
@@ -26,11 +31,12 @@ router.post('/reg/login', async (req, res) => {
     }
 })
 
-router.get('/reg/me', async (req, res) => {
+router.get('/reg/me', auth, async (req, res) => {
     res.send(req.user)
 })
 
-router.patch('/reg/me', async (req, res) => {
+//Update Profile
+router.patch('/reg/me', auth, async (req, res) => {
     const update = Object.keys(req.body)
     const allowedUpdates = ['name', 'password']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -49,23 +55,34 @@ router.patch('/reg/me', async (req, res) => {
         }
 })
 
+// logOut
+router.post('/reg/logout', auth, async(req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
 
+        res.send("Logged Out")
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+// forget password
 router.post('/reg/forget', async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['password']
-        try {
-            const user = User.findOneAndUpdate({email: req.body.email}, {password: req.body.password}, {new: true})
-            .then((newUser) => {
-                console.log(newUser)
-                res.status(200).send(newUser)
-            })
-            .catch((error) => {
-                return res.status(400).send("User With Given Email Doesn't Exist!" + error)
-            })
-        }
-        catch (error) {
-            console.log(error)
-            res.status(500).send(error)
-        }
-    })
+    try {
+        await User.updateOne({ email: req.body.email }, { password: req.body.password })
+        const user = await User.findOne({ email: req.body.email })
+        res.status(200).send(user)
+        if (!user)
+            return res.status(400).send("User With Given Email Doesn't Exist!")
+    } catch (e) {
+        console.log(e)
+        res.status(500).send(e)
+    }
+})
 
+module.exports = router
