99f39f041a6c533553f9dba4503a323f08295755
aryan21999 <itsaryangupta1999@gmail.com>
2021-10-11 12:39:08 +0530

    chat Schema

---
+++ models/chat.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const chatSchema = new mongoose.Schema({
+    sender: {
+        type: String,
+        required: true,
+    },
+    receiver: {
+        type: String,
+        required: true,
+    },
+    message: {
+        type: String,
+        required: true,
+    }
+}, {
+    timestamps: true
+})
+
+const Chat = mongoose.model('Chat', chatSchema)
+
+module.exports = Chat
