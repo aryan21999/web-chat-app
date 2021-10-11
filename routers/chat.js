eddbe5b7100fd750e3ffe4e2e3c233922e2d5e7f
aryan21999 <itsaryangupta1999@gmail.com>
2021-10-11 12:49:04 +0530

    chat Route

---
+++ routers/chat.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const Chat = require('../models/chat')
+const auth = require('../middleware/auth')
+const router = new express.Router()
+
+// Create Chats
+router.post('/chats', auth, async(req, res) => {
+    const chat = new Chat({
+        sender: req.user.email,
+        receiver: req.body.friend,
+        message: req.body.message
+    })
+    try {
+        await chat.save()
+        res.status(201).send(chat)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+// List All Chats
+router.get('/chats', auth, async(req, res) => {
+    try {
+        const chat = await Chat.find()
+            // const chat = await Chat.find({$or:[{sender: ${sender}}, {receiver: ${receiver}}]}, $and:[{sender: ${sender}}, {receiver: ${receiver}}])
+        res.send(chat)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+module.exports = router
