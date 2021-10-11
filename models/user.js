7ef7e80685189e347459bc487c2a58299fafbb76
aryan21999 <itsaryangupta1999@gmail.com>
2021-10-11 12:23:09 +0530

    some changes in user Schema

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -29,8 +30,22 @@ const userSchema = new mongoose.Schema({
                 throw new Error('Password cannot contain "password"')
             }
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 })
+
+userSchema.method.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisismyfirstproject')
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+    return token
+}
 
 const User = mongoose.model('User', userSchema)
 
